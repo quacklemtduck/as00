9a1a5ff5680590cc5ea52f758a7ba64e25af3bd3
Andreas <anjp@itu.dk>
2021-09-08 14:52:02 +0200

    Dev (#2)
    * Made HelloWorld
    
    * Tested and implemented IsLeapYear
    
    * Created user interface for IsLeapYear
    
    * Made Errorhandling for wrong inputs
    
    * Made tests for error handling
    
    * Update gitignore
    
    * Added documentation

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -1,14 +1,73 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace MyApp.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void IsDivisibleByFourTest()
         {
+            Assert.False(Program.IsLeapYear(2021));
+            Assert.True(Program.IsLeapYear(2020));
+            Assert.True(Program.IsLeapYear(2004));
+            Assert.False(Program.IsLeapYear(2009));
+            Assert.True(Program.IsLeapYear(2008));
+        }
 
+        [Fact]
+        public void IsDivisibleBy100Test()
+        {
+            Assert.False(Program.IsLeapYear(1800));
+            Assert.False(Program.IsLeapYear(2100));
+        }
+
+        [Fact]
+        public void IsDivisibleBy400Test()
+        {
+            Assert.True(Program.IsLeapYear(1600));
+            Assert.True(Program.IsLeapYear(2000));
+        }
+
+        [Fact]
+        public void CorrectInputTest()
+        {
+            StringReader input = new StringReader("2000");
+            Console.SetIn(input);
+            StringWriter output = new StringWriter();
+            Console.SetOut(output);
+            Program.Main();
+            string actual = output.ToString().Split(Environment.NewLine)[1];
+            string expected = "yay";
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void NonIntegerInputTest()
+        {
+            StringReader input = new StringReader("Hello, World!");
+            Console.SetIn(input);
+            StringWriter output = new StringWriter();
+            Console.SetOut(output);
+            Program.Main();
+            string actual = output.ToString().Split(Environment.NewLine)[1];
+            string expected = "The input is not a number";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TooLowInputTest()
+        {
+            StringReader input = new StringReader("10");
+            Console.SetIn(input);
+            StringWriter output = new StringWriter();
+            Console.SetOut(output);
+            Program.Main();
+            string actual = output.ToString().Split(Environment.NewLine)[1];
+            string expected = "The input must be 1582 or higher";
+            Assert.Equal(expected, actual);
         }
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,11 +2,42 @@
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write a year:");
+            string answer = Console.ReadLine();
+            try{
+                int answerInt = int.Parse(answer);
+                Console.WriteLine(IsLeapYear(answerInt) ? "yay" : "nay");
+            }catch(FormatException e){
+                Console.WriteLine("The input is not a number");
+            }catch(InputYearTooLowException e){
+                Console.WriteLine("The input must be 1582 or higher");
+            }catch(Exception e){
+                Console.WriteLine("Something went wrong");
+            }
+        }
+
+        public static bool IsLeapYear(int year){
+            if(year < 1582) throw new InputYearTooLowException();
+            if(year % 4 == 0){
+                if(year % 100 == 0 && year % 400 != 0) return false;
+                return true;
+            }
+            return false;
+        }
+
+        [System.Serializable]
+        public class InputYearTooLowException : System.Exception
+        {
+            public InputYearTooLowException() { }
+            public InputYearTooLowException(string message) : base(message) { }
+            public InputYearTooLowException(string message, System.Exception inner) : base(message, inner) { }
+            protected InputYearTooLowException(
+                System.Runtime.Serialization.SerializationInfo info,
+                System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
         }
     }
 }
