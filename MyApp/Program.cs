fc27b2d2f9824a9c7b5fa100fcd0afc81afe2653
anjp <anjp@itu.dk>
2021-09-08 13:38:54 +0200

    Made Errorhandling for wrong inputs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -13,17 +13,31 @@ namespace MyApp
                 Console.WriteLine(IsLeapYear(answerInt) ? "yay" : "nay");
             }catch(FormatException e){
                 Console.WriteLine("The input is not a number");
+            }catch(TooLowInputYearException e){
+                Console.WriteLine("The input must be 1582 or higher");
             }catch(Exception e){
                 Console.WriteLine("Something went wrong");
             }
         }
 
         public static bool IsLeapYear(int year){
+            if(year < 1582) throw new TooLowInputYearException();
             if(year % 4 == 0){
                 if(year % 100 == 0 && year % 400 != 0) return false;
                 return true;
             }
             return false;
+        }
+
+        [System.Serializable]
+        public class TooLowInputYearException : System.Exception
+        {
+            public TooLowInputYearException() { }
+            public TooLowInputYearException(string message) : base(message) { }
+            public TooLowInputYearException(string message, System.Exception inner) : base(message, inner) { }
+            protected TooLowInputYearException(
+                System.Runtime.Serialization.SerializationInfo info,
+                System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
         }
     }
 }
