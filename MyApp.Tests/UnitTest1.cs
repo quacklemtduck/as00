3b22595abb070c8f590cbfad37dc3d160a35c86e
anjp <anjp@itu.dk>
2021-09-08 13:03:44 +0200

    Tested and implemented IsLeapYear

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -6,9 +6,27 @@ namespace MyApp.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void IsDivisibleByFourTest()
         {
+            Assert.False(Program.IsLeapYear(2021));
+            Assert.True(Program.IsLeapYear(2020));
+            Assert.True(Program.IsLeapYear(2004));
+            Assert.False(Program.IsLeapYear(2009));
+            Assert.True(Program.IsLeapYear(2008));
+        }
 
+        [Fact]
+        public void IsDivisibleBy100Test()
+        {
+            Assert.False(Program.IsLeapYear(1800));
+            Assert.False(Program.IsLeapYear(2100));
+        }
+
+        [Fact]
+        public void IsDivisibleBy400Test()
+        {
+            Assert.True(Program.IsLeapYear(1600));
+            Assert.True(Program.IsLeapYear(2000));
         }
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,11 +2,20 @@
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Program p = new Program();
+        }
+
+        public static bool IsLeapYear(int year){
+            if(year % 4 == 0){
+                if(year % 100 == 0 && year % 400 != 0) return false;
+                return true;
+            }
+            return false;
         }
     }
 }
